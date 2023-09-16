253398551c8be01d69642cc185cae2d87b3ef012
Ali Salavati <salavati.cuber@gmail.com>
2023-09-16 13:29:54 +0330

    1.0.3

---
+++ src/WaMessenger.php
@@ -0,0 +1,67 @@
+<?php
+namespace Salavati\WaMessenger;
+
+use Salavati\WaMessengerException\WaMessengerException;
+
+class WaMessenger {
+    public $timeout = 30;
+    private $apiKey = null;
+    private $receivers = [];
+
+    /**
+     * @throws WaMessengerException
+     */
+    public function sendMessage($text, $fileUrl = null) {
+        if (empty($this->receivers)) throw new WaMessengerException('شماره دریافت‌کنندگان خالی است.');
+        if (empty($text) && empty($fileUrl)) throw new WaMessengerException('متن پیام و نشانی فایل خالی هستند.');
+        $dataToSend = [
+            'apikey' => $this->apiKey,
+            'text' => $text,
+            'phonenumber' => $this->receivers,
+        ];
+        if (!empty($fileUrl)) $dataToSend['url'] = $fileUrl;
+        $response = $this->sendCurlRequest("https://api.wamessenger.ir/sendMessage/{$this->apiKey}", $dataToSend);
+        return count($this->receivers) == 1 ? [['phonenumber' => $this->receivers[0], 'tracking_code']] : json_decode($response, true);
+    }
+
+    public function setApiKey($apiKey) {
+        $this->apiKey = $apiKey;
+        return $this;
+    }
+
+    public function setReceivers($receivers) {
+        if (is_array($receivers)) $this->receivers = $receivers;
+        if (is_string($receivers)) $this->receivers = [$receivers];
+        return $this;
+    }
+
+    /**
+     * @throws WaMessengerException
+     */
+    private function sendCurlRequest($address, $data) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $address,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => $this->timeout,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => [
+                "cache-control: no-cache",
+                "content-type: application/json"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        if ($err) throw new WaMessengerException("cURL Error #:" . $err);
+
+        curl_close($curl);
+        return $response;
+    }
+
+}
---
+++ src/WaMessengerException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Salavati\WaMessengerException;
+
+use Exception;
+
+class WaMessengerException extends Exception {
+}
