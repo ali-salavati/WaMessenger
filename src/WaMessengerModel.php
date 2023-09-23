246054d27e3e4d5cda334820fc2228fb2c3e2fb1
Ali Salavati <salavati.cuber@gmail.com>
2023-09-23 16:14:29 +0330

    Set API key in constructor.

--- src/WaMessengerModel.php
+++ src/WaMessengerModel.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUnused */
 
 namespace Salavati\WaMessenger;
 
@@ -6,6 +6,10 @@ class WaMessengerModel {
     protected $apiKey = null;
     protected $receivers = [];
     protected $webhook = '';
+
+    public function __construct($apiKey = null) {
+        $this->apiKey = $apiKey;
+    }
 
     public function setApiKey($apiKey) {
         $this->apiKey = $apiKey;
