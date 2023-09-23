53e8d3637e2c5acdb58c6d8a39f2d599a5318b78
Ali Salavati <salavati.cuber@gmail.com>
2023-09-23 10:49:50 +0330

    Set webhook and receive webhook pending.

--- src/WaMessenger.php
+++ src/WaMessenger.php
@@ -7,6 +7,8 @@ class WaMessenger {
     public $timeout = 30;
     private $apiKey = null;
     private $receivers = [];
+    private $webhook = '';
+    public $response = null;
 
     /**
      * @throws WaMessengerException
@@ -23,6 +25,30 @@ class WaMessenger {
         return $this->sendCurlRequest("https://api.wamessenger.ir/sendMessage/{$this->apiKey}", $dataToSend);
     }
 
+    /**
+     * @throws WaMessengerException
+     */
+    public function setWebhook($webhook) {
+        if (!filter_var($webhook, FILTER_VALIDATE_URL)) throw new WaMessengerException('URL is not valid');
+        $this->webhook = $webhook;
+        $url = "https://api.wamessenger.ir/webhook/set/{$this->apiKey}?url={$this->webhook}";
+        $response = $this->sendCurlRequest($url, [], 'GET');
+        $response = json_decode($response);
+        if (!$response || $response->set != 'true') throw new WaMessengerException(empty($response->message) ? 'URL was not set correctly.' : $response->message);
+        return $this;
+    }
+
+    /**
+     * @throws WaMessengerException
+     */
+    public function receiveWebhookPending() {
+        $url = "https://api.wamessenger.ir/webhook/pending/{$this->apiKey}";
+        $response = $this->sendCurlRequest($url, [], 'GET');
+        $result = json_decode($response);
+        if (!$result) throw new WaMessengerException('Unknown Error.');
+        return (int) $result->pending;
+    }
+
     public function setApiKey($apiKey) {
         $this->apiKey = $apiKey;
         return $this;
@@ -37,7 +63,7 @@ class WaMessenger {
     /**
      * @throws WaMessengerException
      */
-    private function sendCurlRequest($address, $data) {
+    private function sendCurlRequest($address, $data = [], $method = 'POST') {
         $curl = curl_init();
 
         curl_setopt_array($curl, [
@@ -47,7 +73,7 @@ class WaMessenger {
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => $this->timeout,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_CUSTOMREQUEST => $method,
             CURLOPT_POSTFIELDS => http_build_query($data),
             CURLOPT_HTTPHEADER => [
                 "cache-control: no-cache",
@@ -55,12 +81,12 @@ class WaMessenger {
             ],
         ]);
 
-        $response = curl_exec($curl);
+        $this->response = curl_exec($curl);
         $err = curl_error($curl);
         if ($err) throw new WaMessengerException("cURL Error #:" . $err);
         curl_close($curl);
-        if ($response === 'Bad Request') throw new WaMessengerException('Bad Request');
-        return $response;
+        if ($this->response === 'Bad Request') throw new WaMessengerException('Bad Request');
+        return $this->response;
     }
 
 }
