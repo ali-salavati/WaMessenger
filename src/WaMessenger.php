46d494c26b684e9cb23e758bae80e4077f30af91
Ali Salavati <salavati.cuber@gmail.com>
2024-11-26 09:28:40 +0330

    Remove the variable $debug and add $log parameter in methods.

--- src/WaMessenger.php
+++ src/WaMessenger.php
@@ -6,12 +6,11 @@ namespace Salavati\WaMessenger;
 class WaMessenger extends WaMessengerModel {
     use WaMessengerCurl;
     private $domain = 'https://api.360messenger.com';
-    public $debug = false;
 
     /**
      * @throws WaMessengerException
      */
-    public function sendMessage($text, $fileUrl = null) {
+    public function sendMessage($text, $fileUrl = null, $log = null) {
         if (empty($this->receivers)) throw new WaMessengerException('At least one receiver phone number is required.');
         if (empty($text) && empty($fileUrl)) throw new WaMessengerException('Message text and file URL are empty. At least one of them is required.');
         $dataToSend = [
@@ -20,23 +19,27 @@ class WaMessenger extends WaMessengerModel {
             'phonenumber' => $this->receivers,
         ];
         if (!empty($fileUrl)) $dataToSend['url'] = $fileUrl;
-        if ($this->debug) error_log(__FILE__ . ':' . __LINE__ . ' ' . json_encode($dataToSend));
-        $response = $this->sendCurlRequest("{$this->domain}/sendMessage/{$this->apiKey}", $dataToSend, 'POST');
-        if ($this->debug) error_log(__FILE__ . ':' . __LINE__ . ' ' . json_encode($response));
-        $result = json_decode($response);
-        if (!$result) throw new WaMessengerException('Is not JSON: ' . $response);
-        return $result;
+        $address = "{$this->domain}/sendMessage/{$this->apiKey}";
+        if ($log) {
+            $log($address, __FILE__, __LINE__);
+            $log($dataToSend, __FILE__, __LINE__);
+        }
+        $response = $this->sendCurlRequest($address, $dataToSend, 'POST');
+        if ($log) $log($response, __FILE__, __LINE__);
+        return $response;
     }
 
     /**
      * @throws WaMessengerException
      */
-    private function sendRequest($url, $isSuccess) {
+    private function sendRequest($url, $isSuccess, $log = null) {
         $response = $this->sendCurlRequest($url, []);
-        if ($this->debug) error_log(__FILE__ . ':' . __LINE__ . ' ' . json_encode($response));
+        if ($log) $log($response, __FILE__, __LINE__);
         if ($response == 'No Pending Message') return [];
         $result = json_decode($response);
+        if ($log) $log($result, __FILE__, __LINE__);
         $success = $result && $isSuccess($result);
+        if ($log) $log($success, __FILE__, __LINE__);
         if (!$success) throw new WaMessengerException(isset($result->message) ? $result->message : "Unknown Error. Server response: {$this->response}");
         return $result;
     }
@@ -44,16 +47,18 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function receiveMessageStatus($messageId) {
+    public function receiveMessageStatus($messageId, $log = null) {
         if (empty($messageId)) throw new WaMessengerException('Message ID is required.');
-        $url = "{$this->domain}/getStatus/{$this->apiKey}?id={$messageId}";
-        return $this->sendRequest($url, function ($result) { return !empty($result->pageCount); });
+        $address = "{$this->domain}/getStatus/{$this->apiKey}?id={$messageId}";
+        if ($log) $log($address, __FILE__, __LINE__);
+        $response = $this->sendRequest($address, function ($result) { return !empty($result->pageCount); });
+        if ($log) $log($response, __FILE__, __LINE__);
     }
 
     /**
      * @throws WaMessengerException
      */
-    public function receiveAllMessagesStatus($phoneNumber, $page = 1) {
+    public function receiveAllMessagesStatus($phoneNumber, $page = 1, $log = null) {
         if (empty($phoneNumber)) throw new WaMessengerException('Phone number is required.');
         $url = "{$this->domain}/showAllMessages/{$this->apiKey}?phonenumber={$phoneNumber}&page={$page}";
         return $this->sendRequest($url, function ($result) { return strtolower($result->set) == 'true'; });
@@ -62,7 +67,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function receivePendingMessages() {
+    public function receivePendingMessages($log = null) {
         $url = "{$this->domain}/pending/{$this->apiKey}";
         return $this->sendRequest($url, function () { return true; });
     }
@@ -70,7 +75,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function receiveAllMessages($phoneNumber, $page = 1) {
+    public function receiveAllMessages($phoneNumber, $page = 1, $log = null) {
         if (empty($phoneNumber)) throw new WaMessengerException('Phone number is required.');
         $url = "{$this->domain}/showAllGetMessages/{$this->apiKey}?phonenumber={$phoneNumber}&page={$page}";
         return $this->sendRequest($url, function ($result) { return  !empty($result->pageCount); });
@@ -79,7 +84,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function setWebhook($webhook) {
+    public function setWebhook($webhook, $log = null) {
         if (!filter_var($webhook, FILTER_VALIDATE_URL)) throw new WaMessengerException('URL is not valid');
         $this->webhook = $webhook;
         $url = "{$this->domain}/webhook/set/{$this->apiKey}?url={$this->webhook}";
@@ -89,7 +94,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function removeWebhook() {
+    public function removeWebhook($log = null) {
         $url = "{$this->domain}/webhook/remove/{$this->apiKey}";
         return $this->sendRequest($url, function ($result) { return strtolower($result->set) == 'true'; });
     }
@@ -97,7 +102,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function receiveWebhookPending() {
+    public function receiveWebhookPending($log = null) {
         $url = "{$this->domain}/webhook/pending/{$this->apiKey}";
         return $this->sendRequest($url, function () { return true; });
     }
@@ -105,7 +110,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function resendWebhook() {
+    public function resendWebhook($log = null) {
         $url = "{$this->domain}/webhook/resend/{$this->apiKey}";
         return $this->sendRequest($url, function () { return true; });
     }
@@ -113,7 +118,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function sendSeen() {
+    public function sendSeen($log = null) {
         $url = "{$this->domain}/sendSeen/{$this->apiKey}?seen=on";
         return $this->sendRequest($url, function ($result) { return strtolower($result->set) == 'true'; });
     }
@@ -121,7 +126,7 @@ class WaMessenger extends WaMessengerModel {
     /**
      * @throws WaMessengerException
      */
-    public function enableReceiveMessage($enable = true) {
+    public function enableReceiveMessage($enable = true, $log = null) {
         $onOrOff = $enable ? 'on' : 'off';
         $url = "{$this->domain}/receive/{$this->apiKey}?receive={$onOrOff}";
         return $this->sendRequest($url, function ($result) { return strtolower($result->set) == 'true'; });
